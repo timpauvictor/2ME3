c43a3315bb4970e0810e1773dfe0806fb3b5aacb
Victor Timpau <timpauvictor@gmail.com>
2016-02-18 13:38:23 -0500

    added red and blue buttons added quit, check and clear buttons added Jack's WIP board and node classes

---
+++ src/application/BoardController.java
@@ -0,0 +1,41 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.event.ActionEvent;
+
+import javafx.scene.input.MouseEvent;
+
+public class BoardController {
+
+	// Event Listener on Circle.onMouseClicked
+	@FXML
+	public void blueClick(MouseEvent event) {
+		System.out.println("Blue Piece Clicked");
+	}
+	
+	// Event Listener on Circle[#redButton].onMouseClicked
+	@FXML
+	public void redClick(MouseEvent event) {
+		System.out.println("Red Piece Clicked");
+	}
+	
+	// Event Listener on Button.onAction
+	@FXML
+	public void checkButton(ActionEvent event) {
+		System.out.println("Check Button Pressed");
+	}
+	
+	// Event Listener on Button.onAction
+	@FXML
+	public void quitButton(ActionEvent event) {
+		System.out.println("Quit Button Pressed");
+	}
+	
+	// Event Listener on Button.onAction
+	@FXML
+	public void clearButton(ActionEvent event) {
+		System.out.println("Clear Button Pressed");
+	}
+	
+}
--- src/application/Controller.java
+++
@@ -1,7 +0,0 @@
-package application;
-
-/**
- * Created by victor on 2/12/16.
- */
-public class Controller {
-}
